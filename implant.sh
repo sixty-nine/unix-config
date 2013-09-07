42a748eef324c7fb843f92e4271f8d59c073a246
Daniel Barsotti <daniel.barsotti@liip.ch>
2013-09-08 00:49:54 +0100

    Fixed problem in the copy of the vim config

--- implant.sh
+++ implant.sh
@@ -23,7 +23,7 @@ log_info "Setting up vim..."
 
 sudo apt-get install -y -qq vim vim-common vim-runtime
 
-cp -R $SETUP_PATH/assets/vim/vim $HOME_DIR/.vim
+cp -R $SETUP_PATH/assets/vim/vim/* $HOME_DIR/.vim
 cp $SETUP_PATH/assets/vim/vimrc $HOME_DIR/.vimrc
 
 cd $HOME_DIR/.vim/bundle
